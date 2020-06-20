71c665f3d130c47763a57343eff6f14e7826eb30
dennmar <dennismariano3@gmail.com>
2020-06-20 12:27:05 -0700

    Refactor agenda view model to abstract lifecycle management into view model class

--- android/app/src/main/java/com/example/measure/features/agenda/AgendaViewModel.java
+++ android/app/src/main/java/com/example/measure/features/agenda/AgendaViewModel.java
@@ -2,13 +2,10 @@ package com.example.measure.features.agenda;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.LifecycleRegistry;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.measure.features.ViewModel;
 import com.example.measure.models.data.Task;
 import com.example.measure.models.data.User;
 import com.example.measure.models.login.LoginRepository;
@@ -24,12 +21,11 @@ import java.util.List;
  * A view model that handles the interaction between the agenda view (fragment)
  * and the model.
  */
-public class AgendaViewModel implements LifecycleOwner {
+public class AgendaViewModel extends ViewModel {
     private TaskRepository taskRepo;
     private LoginRepository loginRepo;
     private UserRepository userRepo;
     private Bundle savedInstanceState;
-    private LifecycleRegistry lifecycleRegistry;
 
     private User currUser;
     private MutableLiveData<List<Task>> sortedTasks;
@@ -56,19 +52,6 @@ public class AgendaViewModel implements LifecycleOwner {
         currUser = this.loginRepo.getCurrentUser();
         sortedTasks = new MutableLiveData<>();
         activeTask = new MutableLiveData<>();
-        lifecycleRegistry = new LifecycleRegistry(this);
-        lifecycleRegistry.setCurrentState(Lifecycle.State.CREATED);
-    }
-
-    /**
-     * Return the lifecycle of this instance.
-     *
-     * @return the lifecycle of this instance
-     */
-    @NonNull
-    @Override
-    public Lifecycle getLifecycle() {
-        return lifecycleRegistry;
     }
 
     /**
@@ -95,8 +78,7 @@ public class AgendaViewModel implements LifecycleOwner {
      * @return observable list of tasks for the user sorted by date
      */
     public LiveData<List<Task>> getSortedTasks(Date startDate, Date endDate) {
-        // Start lifecycle to allow observers set by view model to observe.
-        lifecycleRegistry.setCurrentState(Lifecycle.State.STARTED);
+        enableObservers();
 
         taskRepo.getSortedTasks(currUser, startDate, endDate).observe(this,
                 modelSortedTasks -> {
@@ -145,8 +127,7 @@ public class AgendaViewModel implements LifecycleOwner {
      * @return current active task for the user
      */
     public LiveData<Task> getActiveTask() {
-        // Start lifecycle to allow observers set by view model to observe.
-        lifecycleRegistry.setCurrentState(Lifecycle.State.STARTED);
+        enableObservers();
 
         userRepo.getActiveTask().observe(this, activeTask -> {
             this.activeTask.setValue(activeTask);
@@ -170,7 +151,6 @@ public class AgendaViewModel implements LifecycleOwner {
      * Perform clean-up for this instance.
      */
     public void onDestroy() {
-        // Remove all observers the view model has set by ending lifecycle.
-        lifecycleRegistry.setCurrentState(Lifecycle.State.DESTROYED);
+        removeObservers();
     }
 }
