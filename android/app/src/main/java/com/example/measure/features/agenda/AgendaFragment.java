33106a96913f5909f744f6ed3c3d90747d3775b6
dennmar <dennismariano3@gmail.com>
2020-07-02 18:01:09 -0700

    Update agenda fragment to display mock agenda with ability to add mock tasks

--- android/app/src/main/java/com/example/measure/features/agenda/AgendaFragment.java
+++ android/app/src/main/java/com/example/measure/features/agenda/AgendaFragment.java
@@ -4,17 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.measure.R;
 import com.example.measure.di.MeasureApplication;
+import com.example.measure.models.data.AgendaItem;
+
+import org.joda.time.Duration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A fragment that handles the UI for the user's agenda.
  */
 public class AgendaFragment extends Fragment {
     private AgendaViewModel agendaViewModel;
+    private RecyclerView agendaRecycler;
+    private AgendaItemAdapter agendaAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<AgendaItem> agendaItems;
 
     /**
      * Initialize the agenda view model.
@@ -27,6 +40,7 @@ public class AgendaFragment extends Fragment {
                 .getApplicationContext()).appComponent.avmFactory()
                 .create(savedInstanceState);
         super.onCreate(savedInstanceState);
+        agendaItems = new ArrayList<>();
     }
 
     /**
@@ -42,6 +56,8 @@ public class AgendaFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_agenda, container,
                 false);
+        initAgendaDisplay(rootView);
+        initAddTaskBtn(rootView);
         return rootView;
     }
 
@@ -54,5 +70,33 @@ public class AgendaFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         agendaViewModel.onDestroy();
+    }
+
+    /**
+     * Initialize the display for the agenda.
+     *
+     * @param view view for the user interface
+     */
+    private void initAgendaDisplay(View view) {
+        agendaRecycler = view.findViewById(R.id.agenda_recycler);
+        layoutManager = new LinearLayoutManager(getActivity());
+        agendaRecycler.setLayoutManager(layoutManager);
+        agendaAdapter = new AgendaItemAdapter(agendaItems);
+        agendaRecycler.setAdapter(agendaAdapter);
+    }
+
+    /**
+     * Initialize the button for adding tasks.
+     *
+     * @param view view for the user interface
+     */
+    private void initAddTaskBtn(View view) {
+        Button addTaskBtn = view.findViewById(R.id.add_task_button);
+        addTaskBtn.setOnClickListener(v -> {
+            agendaItems.add(new AgendaItem("Test agenda item",
+                    new Duration(3600000 + 1000 * 17)));
+            agendaAdapter.setAgendaItems(agendaItems);
+            agendaAdapter.notifyDataSetChanged();
+        });
     }
 }
---
+++ android/app/src/main/java/com/example/measure/features/agenda/AgendaItemAdapter.java
@@ -0,0 +1,96 @@
+package com.example.measure.features.agenda;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.measure.R;
+import com.example.measure.models.data.AgendaItem;
+
+import java.util.List;
+
+/**
+ * Adapts agenda items for display in a recycler view.
+ */
+public class AgendaItemAdapter
+        extends RecyclerView.Adapter<AgendaItemAdapter.AgendaItemViewHolder> {
+    private List<AgendaItem> agendaItems;
+
+    /**
+     * Descibes an agenda item view and metadata about its place within the
+     * recyclerview.
+     */
+    public static class AgendaItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameText;
+        private TextView timeText;
+
+        /**
+         * Initialize member variables.
+         *
+         * @param v view to represent the agenda item
+         */
+        public AgendaItemViewHolder(View v) {
+            super(v);
+            nameText = v.findViewById(R.id.agenda_item_row_name);
+            timeText = v.findViewById(R.id.agenda_item_row_time);
+        }
+    }
+
+    /**
+     * Initialize member variables.
+     *
+     * @param agendaItems items to be displayed in the agenda
+     */
+    public AgendaItemAdapter(List<AgendaItem> agendaItems) {
+        this.agendaItems = agendaItems;
+    }
+
+    /**
+     * Create view to represent an item.
+     *
+     * @param parent   viewgroup to add the new view to after it's bound to an
+     *                 adapter position
+     * @param viewType view type of the new view
+     * @return
+     */
+    @NonNull
+    @Override
+    public AgendaItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                   int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_agenda_item, parent, false);
+        AgendaItemViewHolder vh = new AgendaItemViewHolder(v);
+        return vh;
+    }
+
+    /**
+     * Update the display of the item at the given position.
+     *
+     * @param holder   viewholder to be updated for item's display
+     * @param position index of item within the data set
+     */
+    @Override
+    public void onBindViewHolder(@NonNull AgendaItemViewHolder holder,
+                                 int position) {
+        holder.nameText.setText(agendaItems.get(position).getName());
+        holder.timeText.setText(agendaItems.get(position).getTimeWorked());
+    }
+
+    /**
+     * Return the total number of items.
+     *
+     * @return amount of items
+     */
+    @Override
+    public int getItemCount() {
+        return agendaItems.size();
+    }
+
+    public void setAgendaItems(List<AgendaItem> agendaItems) {
+        this.agendaItems = agendaItems;
+    }
+}
