924c0addfd471d269be5e434588e58b1fd4a954a
dennmar <dennismariano3@gmail.com>
2020-08-05 18:13:13 -0700

    Add login DAO tests

---
+++ android/app/src/androidTest/java/com/example/measure/LoginDaoTest.java
@@ -0,0 +1,111 @@
+package com.example.measure;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.room.RoomDatabase;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.measure.di.MeasureApplication;
+import com.example.measure.di.components.DaggerTestLoginDaoComponent;
+import com.example.measure.di.components.TestLoginDaoComponent;
+import com.example.measure.models.data.User;
+import com.example.measure.models.login.LoginDao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Run instrumented integration tests on the login DAO (and the Room database).
+ */
+@RunWith(AndroidJUnit4.class)
+public class LoginDaoTest {
+    // Execute background tasks synchronously (to allow LiveData to work).
+    @Rule
+    public TestWatcher rule = new InstantTaskExecutorRule();
+
+    LoginDao loginDao;
+    RoomDatabase testRoomDb;
+    MeasureApplication app = (MeasureApplication) InstrumentationRegistry
+            .getInstrumentation()
+            .getTargetContext()
+            .getApplicationContext();
+
+    /**
+     * Create a new login DAO.
+     */
+    @Before
+    public void initLoginDao() {
+        TestLoginDaoComponent loginDaoComponent =
+                DaggerTestLoginDaoComponent.factory().newAppComponent(app);
+        loginDao = loginDaoComponent.loginDao();
+
+        // Clear all data before each test.
+        testRoomDb = loginDaoComponent.measureRoomDatabase();
+        testRoomDb.clearAllTables();
+    }
+
+    @After
+    public void closeConnections() {
+        testRoomDb.close();
+    }
+
+    /**
+     * Test getting the current user when there is no current user.
+     */
+    @Test
+    public void testNoCurrentUser() {
+        User actualUser = loginDao.getCurrentUser();
+        assertThat(actualUser, equalTo(null));
+    }
+
+    /**
+     * Test setting the current user.
+     */
+    @Test
+    public void testSetCurrentUser() {
+        User testUser = new User("Test", "test@test.com", "password");
+        loginDao.setCurrentUser(testUser);
+
+        User getResult = loginDao.getCurrentUser();
+        assertThat(getResult, equalTo(testUser));
+    }
+
+    /**
+     * Test clearing the login session.
+     */
+    @Test
+    public void testClearSession() {
+        User testUser = new User("Test", "test@test.com", "password");
+        loginDao.setCurrentUser(testUser);
+
+        User getResult = loginDao.getCurrentUser();
+        assertThat(getResult, equalTo(testUser));
+
+        loginDao.clearSession();
+        User getResult2 = loginDao.getCurrentUser();
+        assertThat(getResult2, equalTo(null));
+    }
+
+    /**
+     * Test setting the current user to different users multiple times.
+     */
+    @Test
+    public void testMultipleUserSwitch() {
+        int numUsers = 5;
+
+        for (int i = 0; i < numUsers; i++) {
+            User testUser = new User("User#" + i, "test" + i + "@test.com", "password");
+            loginDao.setCurrentUser(testUser);
+
+            User getResult = loginDao.getCurrentUser();
+            assertThat(getResult, equalTo(testUser));
+        }
+    }
+}
---
+++ android/app/src/main/java/com/example/measure/di/components/TestLoginDaoComponent.java
@@ -0,0 +1,36 @@
+package com.example.measure.di.components;
+
+import com.example.measure.db.MeasureRoomDatabase;
+import com.example.measure.di.MeasureApplication;
+import com.example.measure.di.modules.AssistedInjectModule;
+import com.example.measure.di.modules.ContextModule;
+import com.example.measure.di.modules.prod.LoginDaoModule;
+import com.example.measure.di.modules.test.TestMeasureRoomDatabaseModule;
+import com.example.measure.models.login.LoginDao;
+
+import dagger.BindsInstance;
+import dagger.Component;
+
+/**
+ * A component targeted for login DAO tests that makes Dagger create a graph of
+ * dependencies and exposes objects from the application graph.
+ */
+@Component(modules = {
+        AssistedInjectModule.class,
+        LoginDaoModule.class,
+        ContextModule.class,
+        TestMeasureRoomDatabaseModule.class
+})
+public interface TestLoginDaoComponent {
+    LoginDao loginDao();
+    MeasureRoomDatabase measureRoomDatabase();
+
+    /**
+     * Creates a new TestLoginDaoComponent instance each time it is called.
+     */
+    @Component.Factory
+    interface Factory {
+        TestLoginDaoComponent newAppComponent(
+                @BindsInstance MeasureApplication app);
+    }
+}
