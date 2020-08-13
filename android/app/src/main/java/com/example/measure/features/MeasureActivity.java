3190670b575083f023c4edacd78c5cfb803d2549
dennmar <dennismariano3@gmail.com>
2020-08-13 16:43:49 -0700

    Add tab navigation and habit tracker fragment

--- android/app/src/main/java/com/example/measure/features/MeasureActivity.java
+++ android/app/src/main/java/com/example/measure/features/MeasureActivity.java
@@ -1,9 +1,14 @@
 package com.example.measure.features;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.measure.R;
 import com.example.measure.features.agenda.view.AgendaFragment;
+import com.example.measure.features.habit_tracker.HabitTrackerFragment;
+import com.google.android.material.tabs.TabLayout;
 
 
 /**
@@ -19,7 +24,43 @@ public class MeasureActivity extends FragActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measure);
+        initTabLayout();
+
         setFragContainerId(R.id.measure_act_container);
         addFragment(new AgendaFragment());
+    }
+
+    /**
+     * Set an on tab selected listener on the tab layout.
+     */
+    private void initTabLayout() {
+        TabLayout tl = findViewById(R.id.tab_layout_measure);
+        tl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment nextFrag = null;
+
+                switch (tab.getPosition()) {
+                    case 0:
+                        nextFrag = new AgendaFragment();
+                        break;
+                    case 1:
+                        nextFrag = new HabitTrackerFragment();
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
+                }
+
+                replaceFragment(nextFrag);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
     }
 }
